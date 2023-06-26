795dad24c49d63d588d5f50331ff10292802019f
Pom Pom <2003victoryy@gmail.com>
2023-06-26 10:47:39 +0800

    Delete proxy.php

--- dawndungeon-frontend/templates/proxy.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $url = $_GET['url'];
-    $response = file_get_contents($url);
-    header('Content-Type: application/json');
-    header('Access-Control-Allow-Origin: *');
-    echo $response;
-?>
